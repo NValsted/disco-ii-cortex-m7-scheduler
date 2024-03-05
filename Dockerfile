55e0f6b28b7bfbd7e2a0a7ae91feac06e6a6fd93
NValsted <nicolaj.valsted@live.dk>
2024-03-05 22:08:06 +0100

    change build setup

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,20 @@ RUN apt-get update && apt-get install -y \
     make \
     cmake \
     patch \
+    python3-pip \
+    python3 \
+    ninja-build \
     ca-certificates \
     --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+RUN pip3 install meson
 
 RUN mkdir -p /usr/local/arm
 RUN curl -L "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B" -o gcc-arm-none-eabi.tar.bz2 \
     && tar -xjf gcc-arm-none-eabi.tar.bz2 -C /usr/local/arm \
     && rm gcc-arm-none-eabi.tar.bz2
 ENV ARMGCC_DIR=/usr/local/arm/gcc-arm-none-eabi-10.3-2021.10
+ENV PATH=$ARMGCC_DIR/bin:$PATH
 
 ENV REPO_ROOT=/disco-ii-cortex-m7-scheduler
 
--- src/armgcc/build-service-start.sh
+++ src/armgcc/build-service-start.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 set -e
-set +x
+
+if [ -d "${REPO_ROOT}/lib/lib_output/si" ]; then read -p "Rebuild dependencies? (y/n) " rebuild_dependencies; fi
 
 cp -r /tmp-src/* ${REPO_ROOT}/src
+
+if [[ $rebuild_dependencies != "n" ]]
+then
+    rm -rf ${REPO_ROOT}/lib/lib_output/si
+    mkdir -p /${REPO_ROOT}/lib/lib_output/si
+
+    mkdir -p ${REPO_ROOT}/lib/subprojects
+    cp ${REPO_ROOT}/src/FreeRTOSConfig.h ${REPO_ROOT}/fsimx8mp-m7-sdk/rtos/freertos/freertos_kernel/include/FreeRTOSConfig.h
+    if [[ ! -e ${REPO_ROOT}/lib/subprojects/picolibc ]]; then ln -s ${REPO_ROOT}/lib/picolibc ${REPO_ROOT}/lib/subprojects/picolibc; fi
+    if [[ ! -e ${REPO_ROOT}/lib/subprojects/csp ]]; then ln -s ${REPO_ROOT}/lib/csp ${REPO_ROOT}/lib/subprojects/csp; fi
+    if [[ ! -e ${REPO_ROOT}/lib/subprojects/param ]]; then ln -s ${REPO_ROOT}/lib/param ${REPO_ROOT}/lib/subprojects/param; fi
+    if [[ ! -e ${REPO_ROOT}/lib/csp/freertos ]]; then ln -s ${REPO_ROOT}/fsimx8mp-m7-sdk/rtos/freertos/freertos_kernel ${REPO_ROOT}/lib/csp/freertos; fi
+
+    # Apply temporary csp patch
+    patch ${REPO_ROOT}/lib/csp/meson.build < ${REPO_ROOT}/lib/csp_meson.patch
+
+    cd ${REPO_ROOT}/lib/
+    rm -rf build
+    meson setup \
+        --prefix=${REPO_ROOT}/lib/lib_output/si build \
+        --cross-file ${REPO_ROOT}/lib/cross-arm-none-eabi.txt
+    ninja -C build
+    ninja -C build install
+
+    # Remove temporary csp patch
+    patch -R ${REPO_ROOT}/lib/csp/meson.build < ${REPO_ROOT}/lib/csp_meson.patch
+fi
+
+# Build main project
 cd ${REPO_ROOT}/src/armgcc
 
-# Apply temporary patches
-patch ${REPO_ROOT}/lib/csp/include/csp/csp.h < ${REPO_ROOT}/lib/csp_endian.patch
-
-set +e
 ./build.sh
-set -e
-
-# Remove temporary patches
-patch -R ${REPO_ROOT}/lib/csp/include/csp/csp.h < ${REPO_ROOT}/lib/csp_endian.patch
 
 if [ -d "${REPO_ROOT}/bin/debug" ]; then rm -r ${REPO_ROOT}/bin/debug; fi
 if [ -d "${REPO_ROOT}/bin/release" ]; then rm -r ${REPO_ROOT}/bin/release; fi
